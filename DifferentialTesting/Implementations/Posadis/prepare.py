9a3d572be1e6c4946f8929a22b06ed561a10d0ad
FreddyYJ <kyj1411@unist.ac.kr>
2022-12-06 15:16:02 +0900

    Fix posadis

--- DifferentialTesting/Implementations/Posadis/prepare.py
+++ DifferentialTesting/Implementations/Posadis/prepare.py
@@ -1,8 +1,33 @@
 #!/usr/bin/env python3
 
+import os
 import pathlib
 import subprocess
+from typing import List
 
+def converter(orig_lines:List[str]):
+    result=[]
+    for line in orig_lines:
+        if 'IN SOA' in line:
+            tokens=line.strip().split()
+            new_line=f'@ {tokens[1]} SOA ( . meilof@{tokens[0][:-1]} 2002052301 {tokens[7]} {tokens[8]} {tokens[9]} {tokens[10]} )\n'
+            result.append(new_line)
+        elif 'IN NS' in line:
+            tokens=line.strip().split()
+            new_line=f'{tokens[0]} NS {tokens[4]}\n'
+            result.append(new_line)
+        elif 'IN CNAME' in line:
+            tokens=line.strip().split()
+            new_line=f'{tokens[0]} CNAME {tokens[4]}\n'
+            result.append(new_line)
+        elif 'IN DNAME' in line:
+            tokens=line.strip().split()
+            new_line=f'{tokens[0]} DNAME {tokens[4]}\n'
+            result.append(new_line)
+
+        else: raise ValueError(f'Unknown line: {line}')
+
+    return result
 
 def run(zone_file: pathlib.Path, zone_domain: str, cname: str, port: int, restart: bool, tag: str) -> None:
     """
@@ -24,9 +49,31 @@ def run(zone_file: pathlib.Path, zone_domain: str, cname: str, port: int, restar
         # Kill the running server instance inside the container
         subprocess.run(
             ['docker', 'exec', cname, 'pkill', 'posadis'], check=False)
+
+    # Convert default input to posadis format
+    file_name=f'db.{zone_domain[:-1]}'
+    subprocess.run(['cp','-rf','Implementations/Posadis/posadisrc',f'Implementations/Posadis/rc_{cname}'],stdout=subprocess.PIPE, check=False)
+    with open(f'Implementations/Posadis/rc_{cname}', 'r') as file_pointer:
+        orig_lines=file_pointer.readlines()
+        new_lines=orig_lines.copy()
+        for i,line in enumerate(orig_lines):
+            if line.startswith('ZoneFile'):
+                new_lines[i]=f'ZoneFile "{file_name}"\n'
+    with open(f'Implementations/Posadis/rc_{cname}', 'w') as file_pointer:
+        file_pointer.writelines(new_lines)
+    
+    subprocess.run(['cp','-rf',zone_file,'Implementations/Posadis/'],stdout=subprocess.PIPE, check=False)
+    # with open(zone_file, 'r') as file_pointer:
+    #     orig_lines=file_pointer.readlines()
+    #     new_lines=converter(orig_lines)
+    # with open(f'Implementations/Posadis/{zone_file.name}', 'w') as file_pointer:
+    #     file_pointer.writelines(new_lines)
+
     # Copy the new zone file into the container
-    subprocess.run(['docker', 'cp', str(zone_file), cname +
-                    f':/etc/posadis/zn.{zone_domain}'], stdout=subprocess.PIPE, check=False)
+    subprocess.run(['docker', 'cp', f'Implementations/Posadis/{zone_file.name}', cname +
+                    f':/etc/posadis/db.{zone_domain[:-1]}'], stdout=subprocess.PIPE, check=False)
+    subprocess.run(['docker', 'cp', f'Implementations/Posadis/rc_{cname}', cname +
+                    ':/etc/posadisrc'], stdout=subprocess.PIPE, check=False)
 
     # Start the server
     subprocess.run(['docker', 'exec', '-d', cname, 'posadis'],
